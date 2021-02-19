91bb8b417acd94192dd50c29648b9f3a7ce25eba
bethanbh <78606897+bethanbh@users.noreply.github.com>
2021-02-19 09:36:24 +0000

    Add files via upload
    most recent version!
    two histograms

--- environment.py
+++ environment.py
@@ -6,16 +6,14 @@ Created on Sun Feb 14 15:39:41 2021
 """
 
 
-#import numpy as np
+import numpy as np
 #from scipy.constants import k
 #import matplotlib 
 import matplotlib.pyplot as plt
 #import scipy as scipy
 #from scipy import optimize
 from matplotlib import gridspec
-from matplotlib.animation import FuncAnimation
-import imageio
-import os
+import scipy.stats
 
 from particle import Particle   
      
@@ -29,35 +27,9 @@ class Environment:
         self.omega_x = omega_x
         self.omega_y = omega_y
         self.omega_z = omega_z
-        self.particles = []
             
-    def get_details(self): #this is just to replace the following code until I work out why it's not working
-        print(f'Side length is {self.L}')
-        print(f'Initial Temperature is {self.Ti}')
-        
-    #for some reason, the following code kept killing the kernel when I ran it, not sure why
-    # @property
-    # def Ti(self):
-    #    return self.Ti
-    # @Ti.setter
-    # def Ti(self, value):
-    #    self.Ti = value
-    #@property
-    #def L(self):
-    #    return self.L
-    #@L.setter
-    #def L(self, value):
-    #    self.L = value
-    
-    #creates a single particle, random r and v 
-    def Create_Particle(self):
-        test_p = Particle(1,1,1,1,1,1)
-        Particle.set_rand_rv(test_p,self.L,self.Ti)
-        print(test_p.r) #this is mostly a check that it was doing something
   
-    #attempting to create a function to replicate the 'plot trajectory' function, 
-    #in a more streamlined way ie don't have to type out the thing multiple times,
-    #just specify time step, number of particles and total time
+
     def Create_many_particles(self, N, Nt, dt):
         #this allows multiple particles to be created, can specify the time step and total time loop as well
         #the plotting part was mostly to visually confirm what was happening!
@@ -76,11 +48,8 @@ class Environment:
                 ax1.scatter(test_p.x, test_p.y)
                 Particle.drift(test_p, dt)
                 Particle.potential_v_change(test_p, 2, 2, 2,dt)
-    #definitely not the way to actually do the simulation i don't think, it was mainly 
-    #just to check everything was working so far
-    #THINGS TO FIX- ideally want to have all the dots related to one particle be the
-    #same colour, and also the particles aren't trapped in the box!! maybe incorporate 
-    #the bouncing in the drift function? as that's the part where they actually move
+
+    
     
     def test_sim(self, N, Nt, dt):
         '''N is the number of particles you want to simulate, Nt is the number of timesteps to 
@@ -92,7 +61,6 @@ class Environment:
         v = [[],[],[]]
         save_images = False #set to true if you want to save all the images as they are made
         create_graph = False #set to true if you want to plot the xy positions for each time step
-        
         for n in range(N):
             #looping through the number of particles, creating a particle for each and setting the r and v to be random (gaussian) values
             test_p = Particle(1,1,1,1,1,1)
@@ -104,7 +72,24 @@ class Environment:
             v[0].append(test_p.v[0])
             v[1].append(test_p.v[1])
             v[2].append(test_p.v[2])
-        plt.hist(r[0], bins=100)
+            
+        #attempting to plot a histogram of the initial values (for x at the minute)
+        to_plot = v[0]
+        fig, (ax2, ax3) = plt.subplots(2)
+        result1 = ax2.hist(to_plot, bins=100)
+        ax2.set_xlim(-0.02,0.02)
+        mean1 = np.mean(to_plot)
+        sigma1 = np.sqrt(np.var(to_plot))
+        ax2.text(0.01,10, 'mean {:.3g}'.format(mean1))
+        ax2.text(0.01,20, 'sd {:.3g}'.format(sigma1))
+        ax2.set_title(f'N = {N}, Nt ={Nt}, dt={dt} T={self.Ti}, omega={self.omega_x}')
+        x1 = np.linspace(min(to_plot), max(to_plot), 100)
+        dx1 = result1[1][1] - result1[1][0]
+        scale1 = len(to_plot)*dx1
+        #plotting a Gaussian of the results, display mean and sd of the data on the graph
+        ax2.plot(x1, scipy.stats.norm.pdf(x1, mean1, sigma1)*scale1)
+        
+        
         for t in range(Nt): #looping through all the timesteps
             #create a graph
             if create_graph:
@@ -130,8 +115,21 @@ class Environment:
                 plt.savefig(r'C:\\Users\Bethan\Documents\evaporative cooling\test sim\images\timestep{t}.png'.format(t=t))
         
         
-        #attempting to plot a histogram of the last value
-        plt.hist(r[0], bins=50)
-                        
-env = Environment(0.1,10**-6,20,20,20) #here L= 10cm, T = 1uK, note these freq are the omega, included the 2pi factors 
-env.test_sim(100,10,0.05)
+        #attempting to plot a histogram of the last values (for x at the minute)
+        result = ax3.hist(to_plot, bins=100)
+        ax3.set_xlim(-0.02,0.02)
+        #plotting a Gaussian of the results, display mean and sd of the data on the graph
+        mean = np.mean(to_plot)
+        sigma = np.sqrt(np.var(to_plot))
+        ax3.text(0.01,10, 'mean {:.3g}'.format(mean))
+        ax3.text(0.01,20, 'sd {:.3g}'.format(sigma))
+        x = np.linspace(min(to_plot), max(to_plot), 100)
+        dx = result[1][1] - result[1][0]
+        scale = len(to_plot)*dx
+        ax3.plot(x, scipy.stats.norm.pdf(x, mean, sigma)*scale)
+        
+
+
+                
+env = Environment(0.1,10**-6,40,40,40) #here L= 10cm, T = 1uK, note these freq are the omega, included the 2pi factors 
+env.test_sim(1000,50,0.01)
